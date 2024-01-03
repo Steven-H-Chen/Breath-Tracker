48aa006c871161d21882482fe48e2930dfbdb133
Steven Chen <134465360+Steven-H-Chen@users.noreply.github.com>
2024-01-02 21:41:28 -0800

    Almost completed circle animation
    Able to get the circle to increase in size but still developing the syntax to decrease the size.

--- main.py
+++ main.py
@@ -10,9 +10,7 @@ from kivy.animation import Animation
 from kivy.graphics import Ellipse, Color
 import time
 
-
 class BreathTrackerApp(App):
-
 
     def build(self): #self is a reference to the class name in a way its like saying BreathTrackerApp but instead as self
         layout = BoxLayout(orientation='vertical', padding=100, spacing= 100)
@@ -27,28 +25,34 @@ class BreathTrackerApp(App):
         with container.canvas:
             #self.circle_color = Color(1, 0, 0, 1)  # Initial color (red)
             self.circle = Ellipse(pos=(473, 250), size=(50, 50))
-        container.bind(on_touch_down=self.animate_circle)
+        #container.bind(on_touch_down=self.animate_circle)
         layout.add_widget(container)
 
-        start_button = Button(text='Start Breathing', on_press=self.start_breathing, background_color=(0, 1, 0, 1))
+        start_button = Button(text='Start Breathing', background_color=(0, 1, 0, 1))
+        start_button.bind(on_press=self.start_breathing)
+        start_button.bind(on_press=self.animate_circle)
         layout.add_widget(start_button)        
 
         return layout
     
-    def animate_circle(self, instance, touch):
+    def animate_circle(self, instance ):#touch
         # Animate the circle's position, size, and color
+        bduration = int (self.slider.value)
+        if bduration >0:
+            animate = Animation(size=(100, 100),duration=bduration) #+ Animation(background_color=(0, 1, 0, 1))
+            animate.start(self.circle)
+        elif bduration >0:
+            animate = Animation(size=(100, 100),duration=bduration) #+ Animation(background_color=(0, 1, 0, 1))
+            animate.start(self.circle)
 
-        animate = Animation(size=(100, 100),duration=0.1) + Animation(color=(0, 1, 0, 1))
-        animate.start(self.circle)
-        animate.stop(self.circle)
+        #animate.stop(self.Ellipse)
 
-        self.anim = animate
+
         #pos=(touch.x - 25, touch.y - 25),
 
     def start_breathing(self, instance): # instance in this situation is reference to the trigger method basically referencing on press from the button 
         breath_duration = int(self.slider.value)
         self.breath_label.text = f'Breathe in  {breath_duration} seconds...'
-
 
         # Schedule countdown for breath in phase
         Clock.schedule_interval(self.update_countdown_in, 1)
@@ -57,7 +61,6 @@ class BreathTrackerApp(App):
     def update_countdown_in(self, dt):
         if self.countdown_in > 0:
             self.breath_label.text = f' Breathin: Hold for {self.countdown_in} seconds...'
-            self.anim +=Animation(size=(10,10))
             self.countdown_in -= 1
             
         else:
@@ -77,18 +80,6 @@ class BreathTrackerApp(App):
             self.breath_label.text = 'Done...'
             Clock.unschedule(self.update_countdown_out)
     
-    #def builds(self):
-        #container = Widget()
-        #with container.canvas:
-        #    self.circle_color = Color(1, 0, 0, 1)  # Initial color (red)
-        #   self.circle = Ellipse(pos=(100, 100), size=(50, 50))
-        #container.bind(on_touch_down=self.animate_circle)
-        #return container
-    
-
-
-# here i am trying to make a function that would pass the timer command from the slider into the countdown file so that the command 
-# so that the coundown file makes the countdown occur and displays it back out here each time a count down has been made until its finished
 
 if __name__ == '__main__':
     BreathTrackerApp().run()
