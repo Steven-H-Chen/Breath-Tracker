785fdd36f759ad906bc86183b2f2f86e9f6972d9
Steven Chen <134465360+Steven-H-Chen@users.noreply.github.com>
2024-01-03 21:28:30 -0800

    Complete Circle Animation
    I made the size to negative to simulate the reduction in size to then the expansion in size.

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ class BreathTrackerApp(App):
         container = Widget()#calling in a container widget
         with container.canvas:
             #self.circle_color = Color(1, 0, 0, 1)  # Initial color (red)
-            self.circle = Ellipse(pos=(473, 250), size=(50, 50))
+            self.circle = Ellipse(pos=(473, 320), size=(100, 100))
         #container.bind(on_touch_down=self.animate_circle)
         layout.add_widget(container)
 
@@ -37,15 +37,17 @@ class BreathTrackerApp(App):
     
     def animate_circle(self, instance ):#touch
         # Animate the circle's position, size, and color
-        bduration = int (self.slider.value)
-        if bduration >0:
-            animate = Animation(size=(100, 100),duration=bduration) #+ Animation(background_color=(0, 1, 0, 1))
+        
+        
+        if self.slider.value >0:
+            bduration = int (self.slider.value*2.45)
+            animate = Animation(size=(-100, -100),duration=bduration) #+ Animation(background_color=(0, 1, 0, 1))
             animate.start(self.circle)
-        elif bduration >0:
-            animate = Animation(size=(100, 100),duration=bduration) #+ Animation(background_color=(0, 1, 0, 1))
-            animate.start(self.circle)
-
-        #animate.stop(self.Ellipse)
+            
+        #else:
+            #animate = Animation(size=(-100,-100), duration=bduration)
+            #animate.start(self.circle)
+            
 
 
         #pos=(touch.x - 25, touch.y - 25),
@@ -71,11 +73,14 @@ class BreathTrackerApp(App):
             breath_duration = int(self.slider.value)
             Clock.schedule_interval(self.update_countdown_out, 1)
             self.countdown_out = breath_duration
+            self.slider.value = int(0)
 
     def update_countdown_out(self, dt):
-        if self.countdown_out > 0:
-            self.breath_label.text = f'Breathe out: Hold for {self.countdown_out} seconds...'
-            self.countdown_out -= 1
+        if self.slider.value < self.countdown_out :
+            self.breath_label.text = f'Breathe out: Hold for {self.slider.value} seconds...'
+            self.slider.value +=1
+            #self.countdown_out -= 1
+            
         else:
             self.breath_label.text = 'Done...'
             Clock.unschedule(self.update_countdown_out)
